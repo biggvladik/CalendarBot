7111b1717ba9d51aaf27535df38ee215e2e8bf20
biggvladik <zuevvlad55@gmail.com>
2024-07-07 23:26:29 +0300

    send_push ready!

--- handlers/start.py
+++ handlers/start.py
@@ -1,7 +1,14 @@
 from aiogram import types, Router
 from aiogram.filters import Command
-from db import data
 from keyboards.for_start import get_start_kb
+from keyboards.for_admin import  get_admin_reply
+from db import data
+import datetime
+from factory import get_event_by_name, make_str, make_distrib
+from config import bot
+
+
+
 
 router = Router()
 
@@ -26,3 +33,33 @@ async def start_handler(message: types.Message):
     await message.answer(f'Ваш ID: {message.from_user.id}')
 
 
+@router.message(Command("send_push"))
+async def send_push(message: types.Message):
+    today = datetime.datetime.now() + datetime.timedelta(days=1)
+    today = today.strftime('%d.%m.%Y')
+    all_players = data.select_all_players_new()
+    events = get_event_by_name(today)
+    res = make_distrib(all_players, events)
+    res_s = ''
+    for event in res:
+        if not event['event']:
+            continue
+        try:
+            if make_str(event['event']) == 'Расписание не найдено!':
+                continue
+            await bot.send_message(int(event['id']), make_str(event['event']), parse_mode='HTML',
+                                   reply_markup=get_admin_reply())
+            data.insert_message_logs(today, event)
+            res_s += make_str(event['event'])
+        except:
+            pass
+    if res_s:
+        await message.answer(
+            res_s,
+            parse_mode='HTML'
+        )
+    else:
+        await message.answer(
+            'Ничего отправлено не было!',
+            parse_mode='HTML'
+        )
