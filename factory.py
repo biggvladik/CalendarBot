505ef02052adf9df0ebcb12939548f8f7ad5cf7e
biggvladik <zuevvlad55@gmail.com>
2024-07-10 21:47:01 +0300

    поправил factory.py

--- factory.py
+++ factory.py
@@ -1,5 +1,4 @@
 import gspread
-from datetime import datetime
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
@@ -17,7 +16,7 @@ def get_event_by_name(date: str):
     month_number = date.split('.')[1]
 
     gc = gspread.service_account(filename='credits.json')
-    worksheet = gc.open(get_month(config.directory_id,month_number))
+    worksheet = gc.open(get_month(config.directory_id, month_number))
     worksheet_list = worksheet.worksheets()
     current_res = []
     for sheet in worksheet_list:
@@ -28,40 +27,37 @@ def get_event_by_name(date: str):
             sport_name = item[1]
             date_number = item[0].split('\n')[0]
             day_name = item[0].split('\n')[1]
-            print(item)
             workers = [i.strip() for i in item[7].split('\n')]
             event_name = item[2].replace('\n', ' ')
             time = item[5]
-            if date ==  date_number:
-                current_res.append((date_number, day_name, workers, event_name, time,sport_name))
+            if date == date_number:
+                current_res.append((date_number, day_name, workers, event_name, time, sport_name))
     return current_res
 
 
 def make_str(data: list):
-
     res_s = ''
     for number, s in enumerate(data):
         workers = ', '.join(s[2])
         date = s[0] + ' | ' + s[1] + ' | ' + s[4]
-        date = date.replace('\n',' ')
+        date = date.replace('\n', ' ')
         name_event = s[3]
         res = (f'<b>Дата</b>: {date}\n'
                f'<b>Вид спорта</b>: {s[-1]}\n'
                f'<b>Событие</b>: {name_event}\n'
                f'<b>Работники</b>: {workers}\n')
-        res_s = res_s +res
-       # if number!= len(data)-1:
+        res_s = res_s + res
         res_s = res_s + '-----------------------------------------' + '\n'
     if res_s == '':
         res_s = 'Расписание не найдено!'
     return res_s
 
 
-def get_month(directory_id: str,month_number:str):
-    SCOPES = ['https://www.googleapis.com/auth/drive']
-    SERVICE_ACCOUNT_FILE = 'credits.json'
+def get_month(directory_id: str, month_number: str):
+    scopes = ['https://www.googleapis.com/auth/drive']
+    service_account_file = 'credits.json'
     credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+        service_account_file, scopes=scopes)
     service = build('drive', 'v3', credentials=credentials)
 
     results = service.files().list(pageSize=10,
@@ -70,11 +66,12 @@ def get_month(directory_id: str,month_number:str):
     results['files'] = sorted(results['files'], key=lambda x: x['createdTime'])
     return [i['name'] for i in results['files'][-2:] if month_number in i['name']][0]
 
+
 def get_month_full(directory_id: str):
-    SCOPES = ['https://www.googleapis.com/auth/drive']
-    SERVICE_ACCOUNT_FILE = 'credits.json'
+    scopes = ['https://www.googleapis.com/auth/drive']
+    service_account_file = 'credits.json'
     credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+        service_account_file, scopes=scopes)
     service = build('drive', 'v3', credentials=credentials)
 
     results = service.files().list(pageSize=10,
@@ -85,8 +82,7 @@ def get_month_full(directory_id: str):
     return results
 
 
-
-def make_distrib(players:list,events:list):
+def make_distrib(players: list, events: list):
     for event in events:
         for player in players:
             for player_name in [i.strip().lower() for i in event[2]]:
@@ -96,9 +92,8 @@ def make_distrib(players:list,events:list):
     return players
 
 
-
-def make_result_distrib(events:list):
-    s=''
+def make_result_distrib(events: list):
+    s = ''
     for number, event in enumerate(events):
         status = (lambda x: '+' if x else '-')(event[2])
         s = s + (f'<b>ДАТА</b>: {event[0]}\n'
@@ -106,12 +101,12 @@ def make_result_distrib(events:list):
                  f'<b>СТАТУС</b>: {status}\n')
         if number != len(events) - 1:
             s = s + '----------------\n'
-    if s =='':
+    if s == '':
         return 'Событий в этот день не найдено!'
     return s
 
 
-def make_full_str(string:str):
+def make_full_str(string: str):
     substring = "-----------------------------------------"
 
     # Находим индекс последнего вхождения подстроки в строку
@@ -120,8 +115,5 @@ def make_full_str(string:str):
     # Если подстрока найдена, удаляем ее
     if index != -1:
         new_string = string[:index] + string[index + len(substring):]
-        return  new_string
+        return new_string
     return string
-
-
-#get_event_by_name('12.07.2024')
